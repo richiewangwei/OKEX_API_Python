10c2e80b097b1c6bc174ec0ea3b39a2791ba38c3
richiewangwei <39761404+richiewangwei@users.noreply.github.com>
2018-06-26 15:25:15 +0800

    add get_kline

--- FutureAPIClient.py
+++ FutureAPIClient.py
@@ -50,6 +50,48 @@ class Ticker:
         s += ' contract='
         s += self.contract_type
         return s
+
+
+class KLineSingle:
+    def __init__(self):
+        self.date = ""
+        self.open = 0.0
+        self.high = 0.0
+        self.low  = 0.0
+        self.close= 0.0
+        self.vol  = 0.0
+        self.convertBTC = 0.0
+
+        
+class KLineList:
+    def __init__(self):
+        self.symbol = ""
+        self.contract_type = ""
+        self.type = ""
+        self.size = ""
+        self.since = ""
+        self.klinelist = []
+        
+    def get_str(self):
+        s = ''
+        s += 'KLine\tCount=%d' % len(self.klinelist)
+        s += '\tLastKLine='
+        for klinesinge in self.klinelist:
+            s += '\n'
+            s += 'date='
+            if int(klinesinge.date) > 10 ** 9:
+                dt = datetime.fromtimestamp(int(klinesinge.date))
+                s += dt.isoformat(' ')
+            else:
+                s += '0'
+            s += '  open=%.3f' % klinesinge.open
+            s += '  high=%.3f' % klinesinge.high
+            s += '  low=%.3f' % klinesinge.low
+            s += '  close=%.3f' % klinesinge.close
+            s += '  vol=%d' % klinesinge.vol
+            s += '\tconv_vol=%d' % klinesinge.convertBTC
+        return s
+
     
 
 class FutureAPIClient:
@@ -66,7 +108,7 @@ class FutureAPIClient:
         if symbol:
             params += '&symbol=' + symbol if params else 'symbol=' +symbol
         if contractType:
-            params += '&contract_type=' + contractType if params else 'contract_type=' +symbol
+            params += '&contract_type=' + contractType if params else 'contract_type=' + contractType
         return httpGet(self.__url,FUTURE_TICKER_RESOURCE,params)
 
     def get_ticker(self,symbol,contractType):
@@ -89,6 +131,48 @@ class FutureAPIClient:
             pass
         return ticker
 
+
+
+    #获取OKEx合约K线信息
+    def future_kline(self, symbol, contractType, type, size='', since=''): 
+        FUTURE_KLINE_RESOURCE = "/api/v1/future_kline.do"
+        params = ''
+        if symbol:
+            params += '&symbol=' + symbol if params else 'symbol=' + symbol
+        if contractType:
+            params += '&contract_type=' + contractType if params else 'contract_type=' + contractType
+        if type:
+            params += '&type=' + type if params else 'type=' + type
+        if size:
+            params += '&size=' + size if params else 'size=' + size
+        if since:
+            params += '&since=' + since if params else 'since=' + since
+        return httpGet(self.__url,FUTURE_KLINE_RESOURCE,params)
+
+    
+    def get_kline(self, symbol, contractType, type, size='', since=''):
+        klinelist = KLineList()
+        klinelist.symbol = symbol
+        klinelist.contract_type = contractType
+        klinelist.type = type
+        klinelist.size = size
+        klinelist.since = since
+        try:
+            json_obj = self.future_kline(symbol, contractType, type, size, since)
+            for item in json_obj:
+                klinesingle = KLineSingle()
+                klinesingle.date = int(item[0] / 1000)
+                klinesingle.open = item[1]
+                klinesingle.high = item[2]
+                klinesingle.low = item[3]
+                klinesingle.close = item[4]
+                klinesingle.vol = item[5]
+                klinesingle.convertBTC = item[6]
+                klinelist.klinelist.append(klinesingle)
+        except:
+            pass
+        return klinelist
+                
         
 
     
--- FutureAPIClientTest.py
+++ FutureAPIClientTest.py
@@ -9,7 +9,7 @@ import json
 from FutureConfigure import *
 from FutureAPIClient import Ticker, FutureAPIClient
 
-class TestFutureAPIClient(unittest.TestCase):
+class TestFutureAPIClient_Ticker(unittest.TestCase):
 
 
     def setUp(self):
@@ -110,6 +110,111 @@ class TestFutureAPIClient(unittest.TestCase):
         self.assertGreater(ticker.vol, self.ticker_info['vol_min'])
         self.assertGreater(ticker.contract_id, self.ticker_info['cont_min'])
         self.assertEqual(ticker.unit_amount, self.ticker_info['unit_amnt'])       
+        pass
+
+
+
+
+class TestFutureAPIClient_KLine(unittest.TestCase):
+
+
+    def setUp(self):
+        self.kline_infos = []
+        kline = {'symbol':'btc_usd', 'contract_type':'this_week', 'date_min':1526877182, 'price_min':850.0, 'price_max':85000.0, 'vol_min':1000.0, 'type':'1hour'} # 8500
+        self.kline_infos.append(kline)
+        kline = {'symbol':'btc_usd', 'contract_type':'next_week', 'date_min':1526877182, 'price_min':850.0, 'price_max':85000.0, 'vol_min':1000.0, 'type':'15min'} # 8500
+        self.kline_infos.append(kline)
+        kline = {'symbol':'btc_usd', 'contract_type':'quarter',   'date_min':1526877182, 'price_min':850.0, 'price_max':85000.0, 'vol_min':1000.0, 'type':'1day' } # 8500
+        self.kline_infos.append(kline)
+        kline = {'symbol':'ltc_usd', 'contract_type':'this_week', 'date_min':1526877182, 'price_min':14.0,  'price_max':1400.0,  'vol_min':1000.0, 'type':'1hour'} #  140
+        self.kline_infos.append(kline)
+        kline = {'symbol':'ltc_usd', 'contract_type':'next_week', 'date_min':1526877182, 'price_min':14.0,  'price_max':1400.0,  'vol_min':1000.0, 'type':'15min'} #  140
+        self.kline_infos.append(kline)
+        kline = {'symbol':'ltc_usd', 'contract_type':'quarter',   'date_min':1526877182, 'price_min':14.0,  'price_max':1400.0,  'vol_min':1000.0, 'type':'1day' } #  140
+        self.kline_infos.append(kline)
+        kline = {'symbol':'eth_usd', 'contract_type':'this_week', 'date_min':1526877182, 'price_min':72.0,  'price_max':7200.0,  'vol_min':1000.0, 'type':'1hour'} #  720
+        self.kline_infos.append(kline)
+        kline = {'symbol':'eth_usd', 'contract_type':'next_week', 'date_min':1526877182, 'price_min':72.0,  'price_max':7200.0,  'vol_min':1000.0, 'type':'15min'} #  720
+        self.kline_infos.append(kline)
+        kline = {'symbol':'eth_usd', 'contract_type':'quarter',   'date_min':1526877182, 'price_min':72.0,  'price_max':7200.0,  'vol_min':1000.0, 'type':'1day' } #  720
+        self.kline_infos.append(kline)
+        kline = {'symbol':'etc_usd', 'contract_type':'this_week', 'date_min':1526877182, 'price_min':1.8,   'price_max':180.0,   'vol_min':1000.0, 'type':'1hour'} #   18
+        self.kline_infos.append(kline)
+        kline = {'symbol':'etc_usd', 'contract_type':'next_week', 'date_min':1526877182, 'price_min':1.8,   'price_max':180.0,   'vol_min':1000.0, 'type':'15min'} #   18
+        self.kline_infos.append(kline)
+        kline = {'symbol':'etc_usd', 'contract_type':'quarter',   'date_min':1526877182, 'price_min':1.8,   'price_max':180.0,   'vol_min':1000.0, 'type':'1day' } #   18
+        self.kline_infos.append(kline)
+        kline = {'symbol':'bch_usd', 'contract_type':'this_week', 'date_min':1526877182, 'price_min':130.0, 'price_max':13000.0, 'vol_min':1000.0, 'type':'1hour'} # 1300
+        self.kline_infos.append(kline)
+        kline = {'symbol':'bch_usd', 'contract_type':'next_week', 'date_min':1526877182, 'price_min':130.0, 'price_max':13000.0, 'vol_min':1000.0, 'type':'15min'} # 1300
+        self.kline_infos.append(kline)
+        kline = {'symbol':'bch_usd', 'contract_type':'quarter',   'date_min':1526877182, 'price_min':130.0, 'price_max':13000.0, 'vol_min':1000.0, 'type':'1day' } # 1300
+        self.kline_infos.append(kline)
+        kline = {'symbol':'xrp_usd', 'contract_type':'this_week', 'date_min':1526877182, 'price_min':0.07,  'price_max':7.0,     'vol_min':1000.0, 'type':'1hour'} #  0.7
+        self.kline_infos.append(kline)
+        kline = {'symbol':'xrp_usd', 'contract_type':'next_week', 'date_min':1526877182, 'price_min':0.07,  'price_max':7.0,     'vol_min':1000.0, 'type':'15min'} #  0.7
+        self.kline_infos.append(kline)
+        kline = {'symbol':'xrp_usd', 'contract_type':'quarter',   'date_min':1526877182, 'price_min':0.07,  'price_max':7.0,     'vol_min':1000.0, 'type':'1day' } #  0.7
+        self.kline_infos.append(kline)
+        kline = {'symbol':'eos_usd', 'contract_type':'this_week', 'date_min':1526877182, 'price_min':1.4,   'price_max':140.0,   'vol_min':1000.0, 'type':'1hour'} #   14
+        self.kline_infos.append(kline)
+        kline = {'symbol':'eos_usd', 'contract_type':'next_week', 'date_min':1526877182, 'price_min':1.4,   'price_max':140.0,   'vol_min':1000.0, 'type':'15min'} #   14
+        self.kline_infos.append(kline)
+        kline = {'symbol':'eos_usd', 'contract_type':'quarter',   'date_min':1526877182, 'price_min':1.4,   'price_max':140.0,   'vol_min':1000.0, 'type':'1day' } #   14
+        self.kline_infos.append(kline)
+        kline = {'symbol':'btg_usd', 'contract_type':'this_week', 'date_min':1526877182, 'price_min':5.5,   'price_max':550.0,   'vol_min':1000.0, 'type':'1hour'} #   55
+        self.kline_infos.append(kline)
+        kline = {'symbol':'btg_usd', 'contract_type':'next_week', 'date_min':1526877182, 'price_min':5.5,   'price_max':550.0,   'vol_min':1000.0, 'type':'15min'} #   55
+        self.kline_infos.append(kline)
+        kline = {'symbol':'btg_usd', 'contract_type':'quarter',   'date_min':1526877182, 'price_min':5.5,   'price_max':550.0,   'vol_min':1000.0, 'type':'1day' } #   55
+        self.kline_infos.append(kline)
+        rand_int = random.randrange(len(self.kline_infos))
+        self.kline_info = self.kline_infos[rand_int]
+        pass       
+
+    def test_future_kline(self):
+        future_api_client = FutureAPIClient(Future_REST_URL, Future_Api_Key, Future_Secret_Key)
+        json_obj = future_api_client.future_kline(self.kline_info['symbol'], self.kline_info['contract_type'], self.kline_info['type'])
+        self.assertGreater(len(json_obj), 50)
+        #print(json.dumps(json_obj[:3], indent=4))
+        #print(json.dumps(json_obj[-3:], indent=4))
+        last_kline = json_obj[-2]
+        self.assertGreater(int(last_kline[0]), self.kline_info['date_min'])
+        self.assertGreater(float(last_kline[1]), self.kline_info['price_min'])
+        self.assertGreater(float(last_kline[2]), self.kline_info['price_min'])
+        self.assertGreater(float(last_kline[3]), self.kline_info['price_min'])
+        self.assertGreater(float(last_kline[4]), self.kline_info['price_min'])
+        self.assertLess(float(last_kline[1]), self.kline_info['price_max'])
+        self.assertLess(float(last_kline[2]), self.kline_info['price_max'])
+        self.assertLess(float(last_kline[3]), self.kline_info['price_max'])
+        self.assertLess(float(last_kline[4]), self.kline_info['price_max'])
+        self.assertGreater(float(last_kline[5]), self.kline_info['vol_min'])
+        pass
+        
+        
+    def test_get_kline(self):
+        future_api_client = FutureAPIClient(Future_REST_URL, Future_Api_Key, Future_Secret_Key)
+        kline = future_api_client.get_kline(self.kline_info['symbol'], self.kline_info['contract_type'], self.kline_info['type'])
+        s = kline.get_str()
+        self.assertNotEqual(s, '')
+        self.assertGreater(len(s), 50)
+        #print(s[:400])
+        #print(s[-400:])
+        self.assertEqual(kline.symbol, self.kline_info['symbol'])
+        self.assertEqual(kline.contract_type, self.kline_info['contract_type'])
+        self.assertEqual(kline.type, self.kline_info['type'])
+        self.assertGreater(len(kline.klinelist), 50)
+        last_kline = kline.klinelist[-2]
+        self.assertGreater(int(last_kline.date), self.kline_info['date_min'])
+        self.assertGreater(last_kline.open, self.kline_info['price_min'])
+        self.assertGreater(last_kline.high, self.kline_info['price_min'])
+        self.assertGreater(last_kline.low, self.kline_info['price_min'])
+        self.assertGreater(last_kline.close, self.kline_info['price_min'])
+        self.assertLess(last_kline.open, self.kline_info['price_max'])
+        self.assertLess(last_kline.high, self.kline_info['price_max'])
+        self.assertLess(last_kline.low, self.kline_info['price_max'])
+        self.assertLess(last_kline.close, self.kline_info['price_max'])
+        self.assertGreater(last_kline.vol, self.kline_info['vol_min'])
         pass
 
 
