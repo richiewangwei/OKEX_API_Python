d79051fc8cffeda96527994ff8217d653ca461e7
richiewangwei <39761404+richiewangwei@users.noreply.github.com>
2018-06-26 15:21:57 +0800

    add conf

---
+++ FutureConfigure.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# encoding: utf-8
+# 系统配置参数
+
+#初始化apikey，secretkey,url
+Future_Api_Key = '5fc4e0bf-b96a-426f-8cc6-a7aa20235963'
+Future_Secret_Key = '233F2C5E0137E84F414B3B75082E07F1'
+Future_REST_URL = 'www.okb.com'
