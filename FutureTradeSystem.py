b1f7e509dc69758d9d584b054ae9d9177e05fc13
richiewangwei <39761404+richiewangwei@users.noreply.github.com>
2018-07-09 11:14:36 +0800

    add Simple_Open_Close_System_V1

--- FutureTradeSystem.py
+++ FutureTradeSystem.py
@@ -8,6 +8,7 @@ from FutureAPIClient import Ticker, FutureAPIClient
 from FutureTechnicalIndicator import *
 
 
+
 class FutureTradeSystem:
     def __init__(self):
         self.check_open_close_diff_infos = []
@@ -41,8 +42,7 @@ class FutureTradeSystem:
 
 
 
-
-    def check_Open_Close_Diff(self):
+    def Check_Open_Close_Diff(self):
         future_api_client = FutureAPIClient(Future_REST_URL, Future_Api_Key, Future_Secret_Key)
         while True:
             for i in range(len(self.check_open_close_diff_infos)):
@@ -84,15 +84,98 @@ class FutureTradeSystem:
         
         pass
 
+
+
+    def Simple_Open_Close_Policy_V1(self, open_list, close_list):
+        self.open_list = open_list
+        self.close_list = close_list
+        self.open_close_same_count = 0
+        self.open_close_diff_count = 0
+        self.open_close_all_same_count = 0
+        self.open_close_all_diff_count = 0
+        self.profit_all_same = 0.0
+        self.profit_all_diff = 0.0
+
+        for i in range(1, len(self.close_list)):
+            if (self.close_list[i] > self.close_list[i-1] and self.close_list[i] > self.open_list[i]) or \
+               (self.close_list[i] < self.close_list[i-1] and self.close_list[i] < self.open_list[i]):
+                self.open_close_same_count += 1
+            else:
+                self.open_close_diff_count += 1
+
+        for i in range(1, len(self.close_list)):
+            if (self.close_list[i] > self.close_list[i-1] and self.close_list[i] > self.open_list[i] and self.open_list[i] > self.close_list[i-1]) or \
+               (self.close_list[i] < self.close_list[i-1] and self.close_list[i] < self.open_list[i] and self.open_list[i] < self.close_list[i-1]):
+                self.open_close_all_same_count += 1
+                self.profit_all_same += abs(self.close_list[i] - self.open_list[i])
+            else:
+                self.open_close_all_diff_count += 1
+                self.profit_all_diff += abs(self.close_list[i] - self.open_list[i])
+                
+        s = ''
+        s += 'close_price_count= %d' % len(self.close_list)
+        s += '\top_cl_same_count= %d' % self.open_close_same_count
+        s += '\top_cl_diff_count= %d' % self.open_close_diff_count
+        s += '\tsame_diff_sum= %d' % (self.open_close_same_count + self.open_close_diff_count)
+        s += '\n'
+        s += 'op_cl_all_same= %d' % self.open_close_all_same_count
+        s += '\top_cl_all_diff= %d' % self.open_close_all_diff_count
+        s += '\tprof_all_same= %.2f' % self.profit_all_same
+        s += '\tprof_all_diff  = %.2f' % self.profit_all_diff
+        s += '\tprof_diff/same= %.2f' % (self.profit_all_diff / self.profit_all_same * 100)
+        s += '%'
+        s += '\tprof_diff-same= %.2f' % (self.profit_all_diff - self.profit_all_same)
+        s += '\tclose_avg= %.2f' % (sum(self.close_list) / len(self.close_list))
+        s += '\tprofit_percent= %.2f' % ((self.profit_all_diff - self.profit_all_same) / (sum(self.close_list) / len(self.close_list)) * 100)
+        s += '%'
+        s += '\n'
+
+        return s
+
+
+
+    def Simple_Open_Close_System_V1(self):
+        future_api_client = FutureAPIClient(Future_REST_URL, Future_Api_Key, Future_Secret_Key)
+        while True:
+            for i in range(len(self.check_open_close_diff_infos)):
+                if i % len(self.type_list) == 0:
+                    print('\n\n\n\n\n')
+                    
+                symbol = self.check_open_close_diff_infos[i]['symbol']
+                contract_type = self.check_open_close_diff_infos[i]['contract_type']
+                type = self.check_open_close_diff_infos[i]['type']
+                klinelist = future_api_client.get_kline(symbol, contract_type, type)
+                if len(klinelist.klinelist) < 10:
+                    print(str(self.check_open_close_diff_infos[i]) + '\tFuture_API_Client_ERROR!')
+                    continue
+
+                s = ''
+                s += str(self.check_open_close_diff_infos[i])
+                s += '\n'                
+                tech_indicat = FutureTechnicalIndicator(klinelist)
+                s += self.Simple_Open_Close_Policy_V1(tech_indicat.open_list, tech_indicat.close_list)
+                
+                print(s)
+
+                time.sleep(1)
+            break
+        
+        pass
+
         
 
 
 if __name__ == '__main__':
     future_trade_system = FutureTradeSystem()
 
-    # Check Open_Close_Diff
-    future_trade_system.check_Open_Close_Diff()
+    # Check Open_Close_Diff as Test Benchmark
+    #future_trade_system.Check_Open_Close_Diff()
+
+    # Simple Open Close Policy V1
+    #future_trade_system.Simple_Open_Close_System_V1()
+
+    pass
 
 
-            
-            
+
+
