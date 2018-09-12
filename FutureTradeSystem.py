a741223f7f91bf544b549e894118c29c1675ac8f
richiewangwei <39761404+richiewangwei@users.noreply.github.com>
2018-09-12 14:52:52 +0800

    print str

--- FutureTradeSystem.py
+++ FutureTradeSystem.py
@@ -162,21 +162,25 @@ class FutureTradeSystem:
                             s += ' %d' % (obv_simple_list[i].vol_list[j] / 10000)
                             s += ' %d' % (obv_simple_list[i].obv_list[j] / 10000)
                             s += ' %d' % (obv_simple_list[i].ma_obv_list[j] / 10000)
-                            s += ' %+2d' % order_count_list[j]                        
                             diff_order_count = 0
                             if j == 0:
                                 diff_order_count = order_count_list[j]
                             else:
                                 diff_order_count = order_count_list[j] - order_count_list[j-1]
+                            s += ' %+2d' % order_count_list[j]
+                            s += ' %+2d' % diff_order_count
                             stat_orders = Stat_Orders()
                             closed_profit = stat_orders.trade_orders(order_list, diff_order_count, obv_simple_list[i].close_list[j])
                             opened_profit = stat_orders.profit_opened_orders(order_list, obv_simple_list[i].close_list[j])
                             profit_sum += closed_profit                            
-                            s += '\tprofit_sum=\t%+6.2f' % (profit_sum + opened_profit)
-                            s += '\tclo_prof_all=\t%+6.2f' % profit_sum
                             s += '\tclo_prof_one=\t%+6.2f' % closed_profit
+                            s += '\tProfit_Sum=\t%+6.2f' % (profit_sum + opened_profit)
+                            s += '\tclo_prof_all=\t%+6.2f' % profit_sum
                             s += '\topn_prof_one=\t%+6.2f' % opened_profit
-                            if j % 24 == 0 or j == len(tech_indicat.close_list) - 1:
+                            #s += '\torder_list=%s' % str(order_list)
+                            if j % 24 == 0:
+                                print(s)
+                            if j == len(tech_indicat.close_list) - 1:
                                 print(s)
                     #print()
 
